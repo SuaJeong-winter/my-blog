14434659f1ef409262c9d394136f699836162b05
Jeong Sua <sua.jeong.developer@gmail.com>
2025-03-03 16:11:48 +0900

    build: firestore을 활용한 데이터 생성하기 기능 구현

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { app } from "./firebaseApp";
+import { app, db } from "./firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 // import "react-toastify/dist/ReactTostify.css";
@@ -8,6 +8,7 @@ import Router from "./components/Router";
 import Loader from "./components/Loader";
 
 function App() {
+  console.log(db);
   const auth = getAuth(app);
 
   // auth를 체크하기 전에(initialize하기 전에) loader을 띄워주는 용도
